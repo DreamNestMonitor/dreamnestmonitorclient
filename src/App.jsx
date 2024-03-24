a6887afc9a962e9497dcb7e0a44157e6c9a44838
Daniel Lee <dblee0532@gmail.com>
2024-03-24 17:10:53 -0400

    add logo to webapp

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import SleepDateRangeDropDown from "./components/SleepDateRangeDropDown.jsx";
 
 const App = () => {
     return (
-        <div className={"w-5/6 mx-auto"}>
-            <div className={"text-5xl text-center mt-2 mb-2"}>Dreamnest Monitor</div>
-            <hr />
-            <SleepDateRangeDropDown />
+        <div>
+            <div className={"w-5/6 mx-auto"}>
+                <img src={"/logo.png"} alt={"logo"} className={"w-30 h-24"}/>
+                <div className={"text-5xl text-center mb-2"}>DreamNest Monitor</div>
+                <hr/>
+                <SleepDateRangeDropDown/>
+            </div>
         </div>
     )
 }
