4f83ba061f863acf94eee51a287db9f65c9a793f
Daniel Lee <dblee0532@gmail.com>
2024-03-25 11:27:38 -0400

    handle no correlation part

--- src/components/Correlations.jsx
+++ src/components/Correlations.jsx
@@ -2,11 +2,13 @@ const Correlations = ({ prop }) => {
 
     return (
         <div className={"mt-5 mb-2"}>
-           <div>Temperature correlation on sleep quality: <b>{prop.temperatureCor}%</b></div>
-            <div>Brightness correlation on sleep quality: <b>{prop.brightnessCor}%</b></div>
-            <div>Noise correlation on sleep quality: <b>{prop.noiseCor}%</b></div>
+            <div>Temperature correlation on sleep
+                quality: <b>{prop.temperatureCor === -1 ? "N/A" : prop.temperatureCor}%</b></div>
+            <div>Brightness correlation on sleep
+                quality: <b>{prop.brightnessCor === -1 ? "N/A" : prop.brightnessCor}%</b></div>
+            <div>Noise correlation on sleep quality: <b>{prop.noiseCor === -1 ? "N/A" : prop.noiseCor}%</b></div>
         </div>
-    )
+)
 }
 
 export default Correlations;
