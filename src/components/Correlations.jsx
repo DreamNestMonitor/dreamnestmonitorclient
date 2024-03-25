7b31b7ac39d81ebc88e786b2973d9cc4a1172406
Daniel Lee <dblee0532@gmail.com>
2024-03-25 11:30:54 -0400

    percent only show in number

--- src/components/Correlations.jsx
+++ src/components/Correlations.jsx
@@ -3,10 +3,10 @@ const Correlations = ({ prop }) => {
     return (
         <div className={"mt-5 mb-2"}>
             <div>Temperature correlation on sleep
-                quality: <b>{prop.temperatureCor === -1 ? "N/A" : prop.temperatureCor}%</b></div>
+                quality: <b>{prop.temperatureCor === -1 ? "N/A" : prop.temperatureCor + "%"}</b></div>
             <div>Brightness correlation on sleep
-                quality: <b>{prop.brightnessCor === -1 ? "N/A" : prop.brightnessCor}%</b></div>
-            <div>Noise correlation on sleep quality: <b>{prop.noiseCor === -1 ? "N/A" : prop.noiseCor}%</b></div>
+                quality: <b>{prop.brightnessCor === -1 ? "N/A" : prop.brightnessCor + "%"}</b></div>
+            <div>Noise correlation on sleep quality: <b>{prop.noiseCor === -1 ? "N/A" : prop.noiseCor + "%"}</b></div>
         </div>
 )
 }
